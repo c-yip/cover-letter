394438bf0537f600c82daf94c9043192d0a982d1
Christian <christianyip92@gmail.com>
2022-11-14 09:59:38 -0800

    Add input for title. Edit styling.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   const [data, setData] = useState({
     recipient: "",
     company: "",
+    title: "",
     position: "",
   });
 
@@ -16,6 +17,23 @@ function App() {
   }
 
   //edit these to change paragraphs
+
+  function paragraph1() {
+    return (
+      <p>
+        Please accept my application for the {data.title} position at{" "}
+        {data.company}. Although I may not have professional experience as a{" "}
+        {data.position}, through my years of working as an attorney in a
+        fast-paced, stressful, and demanding environment, I have learned how to
+        self-manage, work proactively, and self-teach to keep up with the
+        ever-changing legal environment. My goal is to apply the analytical,
+        problem-solving, and communication skills I developed as an attorney to
+        a {data.position} position to connect people to technology in a seamless
+        and impactful fashion.
+      </p>
+    );
+  }
+
   function paragraph2() {
     return (
       <p>
@@ -31,22 +49,6 @@ function App() {
     );
   }
 
-  function paragraph1() {
-    return (
-      <p>
-        Please accept my application for the {data.position} position at{" "}
-        {data.company}. Although I may not have professional experience as a{" "}
-        {data.position}, through my years of working as an attorney in a
-        fast-paced, stressful, and demanding environment, I have learned how to
-        self-manage, work proactively, and self-teach to keep up with the
-        ever-changing legal environment. My goal is to apply the analytical,
-        problem-solving, and communication skills I developed as an attorney to
-        a {data.position} position to connect people to technology in a seamless
-        and impactful fashion.
-      </p>
-    );
-  }
-
   return (
     <div className="App">
       <h1>Cover letter generator</h1>
@@ -56,6 +58,8 @@ function App() {
         <input type="text" name="recipient" onChange={handleInputChange} />
         <label htmlFor="company">Company:</label>
         <input type="text" name="company" onChange={handleInputChange} />
+        <label htmlFor="title">Title:</label>
+        <input type="text" name="position" onChange={handleInputChange} />
         <label htmlFor="position">Position:</label>
         <input type="text" name="position" onChange={handleInputChange} />
       </div>
